8bc39d45f1919e1a1bc2cc5136ebd96546700914
Eliseo_Nava_Rodriguez <eliseo.nava@cbtis128.edu.mx>
2024-03-17 03:07:29 +0000

    Nava text

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,49 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Practica1Text1(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class Practica1Text1 extends StatelessWidget {
+  const Practica1Text1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        title: const Text(
+          'Nava',
+          style: TextStyle(color: Colors.white, fontSize: 20.0),
         ),
+        backgroundColor: const Color(0xff514eed),
+      ),
+      body: const Center(
+        child: Text(
+          'Martin',
+          style: TextStyle(
+              fontSize: 40.0, //tama
+              fontWeight: FontWeight.bold, //negrita
+              letterSpacing: 2.0, // interespaciado
+              color: Color(0xff085c1d), // color letra
+              fontFamily: "Poppin"),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+        child: const Text("Click"),
       ),
     );
   }
